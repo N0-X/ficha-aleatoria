3798934b26e04131b03d416874675e6a17fa1778
ususl <ivbsilva714@gmail.com>
2026-03-30 13:54:50 -0300

    fodeo

--- src/screens.ts
+++
@@ -1,9 +0,0 @@
-export function showInitiative() {
-    document.querySelector("#initiativeScreen")?.classList.remove("hidden");
-    document.querySelector("#characterScreen")?.classList.add("hidden");
-    }
-
-export function showCharacter() {
-    document.querySelector("#initiativeScreen")?.classList.add("hidden");
-    document.querySelector("#characterScreen")?.classList.remove("hidden");
-}
--- src/state.ts
+++
@@ -1,9 +0,0 @@
-let currentCharacterId: string | null = null;
-
-export function setCurrentCharacter(id: string) {
-    currentCharacterId = id;
-}
-
-export function getCurrentCharacter() {
-    return currentCharacterId;
-}
